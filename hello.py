b4129dc96a73e0e53f9f0e90198840dbb4ffab8f
Sanjaykannan8 <noobg900@gmail.com>
2025-03-29 16:41:19 +0530

    working

--- hello.py
+++ hello.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "<p> hello world </p>"
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        name = request.form['name']
+        age = "you are eligible to vote" if int(request.form['age'])>18 else "go to school da thambiii"
+        return f"Hello, {name}! \n {age}"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     print("hi")
